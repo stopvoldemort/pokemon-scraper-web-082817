5d4b2df8ddc0f990f502a2d17f56656d9c7d4fa7
stopvoldemort <david.squires1@gmail.com>
2017-09-05 16:43:17 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,32 @@
 class Pokemon
+
+  attr_accessor :name, :type, :id, :db
+
+  def initialize(my_hash)
+    @name = my_hash[:name]
+    @type = my_hash[:type]
+    @db = my_hash[:db]
+    @id = my_hash[:id]
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?)
+    SQL
+
+    db.execute(sql, name, type)
+  end
+
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT * FROM pokemon
+      WHERE pokemon.id = ?
+    SQL
+
+    poke = db.execute(sql, id)
+    Pokemon.new({id: poke[0][0], name: poke[0][1], type: poke[0][2], db: db})
+
+  end
+
 end
